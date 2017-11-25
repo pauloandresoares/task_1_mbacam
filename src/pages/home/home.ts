cbd1b5761f847becb66f7400a7c42262a64d5768
Paulo André Soares de Mendonça <pauloandresoares@gmail.com>
2017-11-25 21:47:52 -0200

    [FEAT] Estrutura inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Camera, CameraOptions} from "@ionic-native/camera";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,35 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  currentPhoto;
 
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public camera : Camera) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TakePicture');
+  }
+
+  getPhoto(type){
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType:this.camera.MediaType.PICTURE,
+      sourceType: type == "picture" ? this.camera.PictureSourceType.CAMERA : this.camera.PictureSourceType.SAVEDPHOTOALBUM,
+      correctOrientation: true
+    };
+
+    this.camera.getPicture(options).then((imageData) => {
+
+      this.currentPhoto = 'data:image/jpeg;base64,' + imageData;
+
+    }, (err) => {
+      // Handle error
+    });
   }
 
 }
